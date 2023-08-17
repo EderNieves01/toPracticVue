faa82168df19801f02ddee421ea29cd74952ac43
EderNieves01 <edernieves09@gmail.com>
2023-08-16 23:04:49 -0400

    ToPracticVue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- doble llaves {{variable}} imprime variables dentro de la etiqueta -->
+<!-- v-bind:style="namevar" esto agrega una variable como atributo -->
+<script setup>
+const name = 'Vue dinamico';
+const styleColor = "color: blue"
+</script>
+
+<template>
+
+  <h1>{{name}}</h1>  
+  <h2 v-bind:style="styleColor">Soy azul</h2>
+  
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
