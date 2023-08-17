c888d8441f233801c6c2ab6e989d7dd46496d613
EderNieves01 <edernieves09@gmail.com>
2023-08-16 23:51:44 -0400

    condicional

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <!-- doble llaves {{variable}} imprime variables dentro de la etiqueta -->
 <!-- v-bind:style="namevar" esto agrega una variable como atributo -->
+<!-- {{ activo ? "Estoy activo" : "Estoy inactivo" }} condicional if de corta funcion -->
 <script setup>
 const name = 'Vue dinamico';
 const styleColor = "color: blue";
 const arrayColores = ["blue", "red", "peru"];
+const activo = true;
 </script>
 
 <template>
@@ -11,6 +13,9 @@ const arrayColores = ["blue", "red", "peru"];
   <h1>{{name.toUpperCase()}}</h1>  
   <h2>{{ arrayColores }}</h2>
   <h2 :style="`color: ${arrayColores[2]}`">Soy peru</h2>
+  <h2>
+    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
+  </h2>
   
 </template>
 
