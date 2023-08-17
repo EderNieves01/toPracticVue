87d679f16dd2c90e4a4067ad6cc4cde8d08a94ed
EderNieves01 <edernieves09@gmail.com>
2023-08-17 00:06:04 -0400

    v-if_v-else-if_else

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <!-- doble llaves {{variable}} imprime variables dentro de la etiqueta -->
 <!-- v-bind:style="namevar" esto agrega una variable como atributo -->
 <!-- {{ activo ? "Estoy activo" : "Estoy inactivo" }} condicional if de corta funcion -->
+<!-- acepta todo lo que tenga un retorno inmediato -->
+
 <script setup>
 const name = 'Vue dinamico';
 const styleColor = "color: blue";
@@ -13,10 +15,15 @@ const activo = true;
   <h1>{{name.toUpperCase()}}</h1>  
   <h2>{{ arrayColores }}</h2>
   <h2 :style="`color: ${arrayColores[2]}`">Soy peru</h2>
-  <h2>
+  <!-- <h2>
     {{ activo ? "Estoy activo" : "Estoy inactivo" }}
-  </h2>
-  
+  </h2> -->
+
+  <h2 v-if="activo === true">Estoy activo</h2>
+  <!-- <h2 v-if="!activo">Estoy activo</h2> si es diferecte a activo o v-else -->
+  <!-- <h2 v-else>Estoy inactivo</h2> tambien tenemos el v-else-if="!activo" -->
+  <h2 v-else-if="activo === false">Estoy inactivo</h2>
+  <h2 v-else>Estoy indeciso</h2>
 </template>
 
 <style>
