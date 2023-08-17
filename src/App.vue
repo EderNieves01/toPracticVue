28e2537a48672aba579c0ad4b8108a38215a52f8
EderNieves01 <edernieves09@gmail.com>
2023-08-17 00:24:43 -0400

    v-for_arrays

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,25 @@
 <!-- no puede contener interrupcion en su ejecucion por ej: un p o un h 
 entre medio pero si dentro de la misma etiqueta -->
 <!-- v-show muestra una etiqueta -->
-<!-- v-for itera sobre una lista de elementos -->
+<!-- v-for itera sobre una lista de elementos como un bucle for normal pasarle la :key con del indice-->
 
 <script setup>
 
 const name = 'Vue dinamico';
-const styleColor = "color: blue";
-const arrayColores = ["blue", "red", "peru"];
-const activo = activo;
+const arrayFrutas = ["pera", "banana", "tomates"];
 
 </script>
 
 <template>
 
-  <h1>{{name.toUpperCase()}}</h1>  
-  <h2>{{ arrayColores }}</h2>
-  <h2 :style="`color: ${arrayColores[2]}`">Soy peru</h2>
-  <!-- <h2>
-    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
-  </h2> -->
+  <h1>{{name.toUpperCase()}}</h1> 
+  <ul>
+    <li v-for="(frutas, index) in arrayFrutas" :key="index">
 
-  <h2 v-if="activo === true">Estoy activo</h2>
-  <!-- <h2 v-if="!activo">Estoy activo</h2> si es diferecte a activo o v-else -->
-  <!-- <h2 v-else>Estoy inactivo</h2> tambien tenemos el v-else-if="!activo" -->
-  <h2 v-else-if="activo === false">
-    <span>Icono</span> <!--dentro de la etiqueta h2 sin interrumir-->
-    Estoy inactivo
-  </h2>
-  <h2 v-else>Estoy indeciso</h2>
+      {{ index }} - {{ frutas }}
 
-  <h2 v-show="activo">activo v-show</h2>
+    </li>
+  </ul>
 
 </template>
 
