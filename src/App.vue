b3248dd4b7934fedb3986fde30edba45a4827367
EderNieves01 <edernieves09@gmail.com>
2023-08-17 00:14:30 -0400

    v-show

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,19 @@
 <!-- v-bind:style="namevar" esto agrega una variable como atributo -->
 <!-- {{ activo ? "Estoy activo" : "Estoy inactivo" }} condicional if de corta funcion -->
 <!-- acepta todo lo que tenga un retorno inmediato -->
+<!-- v-if v-else-if v-else dentro de la etiqueta como condicionales -->
+<!-- no puede contener interrupcion en su ejecucion por ej: un p o un h 
+entre medio pero si dentro de la misma etiqueta -->
+<!-- v-show muestra una etiqueta -->
+<!-- v-for itera sobre una lista de elementos -->
 
 <script setup>
+
 const name = 'Vue dinamico';
 const styleColor = "color: blue";
 const arrayColores = ["blue", "red", "peru"];
-const activo = true;
+const activo = activo;
+
 </script>
 
 <template>
@@ -22,8 +29,14 @@ const activo = true;
   <h2 v-if="activo === true">Estoy activo</h2>
   <!-- <h2 v-if="!activo">Estoy activo</h2> si es diferecte a activo o v-else -->
   <!-- <h2 v-else>Estoy inactivo</h2> tambien tenemos el v-else-if="!activo" -->
-  <h2 v-else-if="activo === false">Estoy inactivo</h2>
+  <h2 v-else-if="activo === false">
+    <span>Icono</span> <!--dentro de la etiqueta h2 sin interrumir-->
+    Estoy inactivo
+  </h2>
   <h2 v-else>Estoy indeciso</h2>
+
+  <h2 v-show="activo">activo v-show</h2>
+
 </template>
 
 <style>
