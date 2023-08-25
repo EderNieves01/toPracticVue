1fde73ec3ef06af1eaf5c41b5d013230fc27a43a
EderNieves01 <edernieves09@gmail.com>
2023-08-24 23:57:51 -0400

    vueOfOptions

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,25 @@
 
-<script setup>
+<script>
 
-//importamos estos para asignarselo a cualquier variable para que sea reactiva
-//computed realizar un computo, propiedades computadas
-import { ref, computed } from 'vue';
-
-const name = 'Vue dinamico';
-const colorP = 'negative';
-//variable o constante reactiva para poder renderizar el valor sin 
-//necesidad de volver a cargar la pagina para que sea mas dinamico
-//sin esto el valor de counter en el html no cambiara aunque puede mostrarse en consola
-
-const counter = ref(0);
-const arrayFavoritos = ref([]);
-// metodos - methods
-
-const increment = () => {
-  counter.value ++;
-}
-
-//puede ir sin corchetes
-const decrement = () => counter.value --;
-
-const reset = () => {
-  counter.value = 0;
-}
-
-//accedemos al valor del array y le add el valor del contador
-const add = () => {
-  arrayFavoritos.value.push(counter.value);  
-}
-
-const blockBtn = computed (() => {
-  //busca en el array y num seran sus propiedades, y cuando num sea igual al
-  //contador, luego una condicion de que si se encunetra el numero en el array
-  //retornara la propiedad true o false para habilitar o desactivar el boton
- const numSearch = arrayFavoritos.value.find(num => num === counter.value);
- console.log(numSearch);
-
- //condicion para esclarecer que el cero puede agregarse solo una ves
- if( numSearch === 0 ) return true;
- return numSearch ? true : false;
-
-// se puede hacer de esta forma pero tendriamos el problema del cero
-//  return numSearch || numSearch === 0;
-});
-
-//clase que retorna una propiedad computada desde el style class
-const classCounter = computed(() =>{
-  if(counter.value === 0){
-    return 'zero'
-  }else if( counter.value > 0){
-    return 'positive'
-  }else{
-    return 'negative'
+// exportamos la data para guardar un objeto que tendra la informacion para nuestras variables
+export default{
+  data: () => ({
+    counter: 10,
+    name: 'Vue dinamico',
+    colorP: 'negative',
+  }),
+  methods: {
+   increment(){
+    this.counter++;
+   },
+   decrement(){
+    this.counter--;
+   },
+   reset(){
+    this.counter = 0;
+   }
   }
-})
+}
 </script>
 
 <template>
@@ -66,19 +30,13 @@ const classCounter = computed(() =>{
 
   <!-- renderizar el h2 para que cambie el 0-->
 
-   <h2 :class="classCounter">{{ counter }}</h2>
+   <h2 >{{ counter }}</h2>
 
   <button class="btn btn-success" @click="increment">increment</button>
   <button class="btn btn-danger" @click="decrement">decrement</button>
   <button class="btn btn-warning" @click="reset">reset</button>
-  <button class="btn btn-primary" @click="add" :disabled="blockBtn">add</button>
-  <br>
-  <ul>
-    <li
-    v-for="(num, index) in arrayFavoritos" :key="index">
-  {{ num }}
-</li>
-  </ul>
+   <br>
+
 </div>
 </template>
 
