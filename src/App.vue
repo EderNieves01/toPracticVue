48b9346c57ad7476bb45ec0da533d6e1087c18b3
EderNieves01 <edernieves09@gmail.com>
2023-08-28 23:18:12 -0400

    propsComponecoDinamido

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,19 @@ export default {
 
   <!-- aqui llamamos el componente card da igual si la primera letra es mayuscula o
   minuscula y cerramos la etiqueta-->
-  <card />
-  <card />
-  <card />
+  <!-- aqui no es necesario usar directiva, vas con ponerle el nombre de la propiedad -->
+  
+  <card url="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg"
+  nombre="Vue.js"/>
+ 
+  <card url="https://cdn.svgporn.com/logos/vuetifyjs.svg?response-content-disposition=attachment%3Bfilename%3Dvuetifyjs.svg"
+  nombre="Vuetify.js"/>
+  <card url="https://cdn.svgporn.com/logos/angular-icon.svg?response-content-disposition=attachment%3Bfilename%3Dangular-icon.svg"
+  nombre="Angular.js"/>
+
+  <card url="https://cdn.svgporn.com/logos/react-query-icon.svg?response-content-disposition=attachment%3Bfilename%3Dreact-query-icon.svg"
+  nombre="React.js"/>
+
 </template>
 
 <style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,18 +4,35 @@ necesarios como el script, el template y el style-->
 <template>
 
 <div>
-    
-    <img src="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg" 
-    alt="vue.js">
+<!-- concepto data bianding, directiva dos puntos inyectar propiedad -->
+    <!-- cambiamos el texto del alt y le pasamos una propiedad nombre -->
+<img :src= "url" alt="nombre">
 
-    <h2>vue.js</h2>
-
-    <a href="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg"
+    <h2>{{nombre}}</h2>
+<!-- podemos pararle la misma url aqui -->
+    <a :href= "url"
     target="_blank">Descargar</a>
   </div>
 
 </template>
 
+<script>
+export default{
+    // con esto mandamos el nombre de la propiedad desde el script hacia el html
+    //podemos ingresar el nombre que querramos y lo leera igual
+    //y podemos pasarle el tipo de dato convirtiendo a objeto
+    // props: ["url"]
+    props: {
+        url: {
+            String,
+        },
+        nombre: {
+            String,
+        }
+    }
+}
+
+</script>
 
 <style  scoped>
 h2{
