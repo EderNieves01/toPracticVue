bdd61f80b4546baabf01120095dda81289f6079f
EderNieves01 <edernieves09@gmail.com>
2023-08-28 23:50:06 -0400

    slotAndGridSistem

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,13 @@
-<!-- componentes con extension .vue -->
-<!-- SFC componentes de un solo archivo son los componentes
+<!-- componentes con extension .vue
+SFC componentes de un solo archivo son los componentes
 necesarios como el script, el template y el style-->
 <template>
 
-<div>
+<!-- podemos exportar el componente llamandolo en la etiqueta con mayuscula
+y aceptar el export, esto crea un nuevo componente en el script por default 
+por ende todo en la pagina desaparece y debemos hacerle un slots osea un hijo al
+componente padre y llamarlo dentro de la etiqueta en el Grid.vue-->
+<grid>
 <!-- concepto data bianding, directiva dos puntos inyectar propiedad -->
     <!-- cambiamos el texto del alt y le pasamos una propiedad nombre -->
 <img :src= "url" alt="nombre">
@@ -12,11 +16,13 @@ necesarios como el script, el template y el style-->
 <!-- podemos pararle la misma url aqui -->
     <a :href= "url"
     target="_blank">Descargar</a>
-  </div>
+</grid>
 
 </template>
 
 <script>
+import Grid from './Grid.vue';
+
 export default{
     // con esto mandamos el nombre de la propiedad desde el script hacia el html
     //podemos ingresar el nombre que querramos y lo leera igual
@@ -29,7 +35,8 @@ export default{
         nombre: {
             String,
         }
-    }
+    },
+    components: { Grid }
 }
 
 </script>
@@ -39,24 +46,16 @@ h2{
     margin-bottom: 10px;
 }
 img{
-    width: 10rem;
+    width: 8rem;
+    height: 8rem;
     padding: 0.5rem;
   }
 
-div{
-    display: inline-block;
-    text-align: center;
-    border: 1px solid #f2f2f2;
-    padding: 1%;
-    border-radius: 5px;
-    margin: 4px;
-}
-
 a{
     background-color: yellow;
-    padding: 0.5rem;
+    padding: 0.3rem;
     border-radius: 5px;
     border: 1px solid #f2f2f2;
-  
+  margin: 2px;
 }
   </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+<slot>
+    <!-- renderizo aqui que solo contendra el sistema gid de mi web 
+    con los estilos dados-->
+
+</slot>
+  </div>      
+    
+</template>
+
+<style scoped>
+div{
+    display: inline-block;
+    text-align: center;
+    border: 1px solid #f2f2f2;
+    padding: 3%;
+    border-radius: 5px;
+    margin: 4px;
+}
+</style>
